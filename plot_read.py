64b7805d033736581e30c54c64bb80d79862819c
nexo <zepengl@gmail.com>
2021-02-04 15:46:21 +0800

    minor update

--- plot_read.py
+++ plot_read.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-wfs = []
 f = open('dumb', 'r')
+linenum = 0
 for line in f:
+    wfs = []
     event = line.split()[5:]
     wf = []
     print(len(event)/32)
@@ -15,11 +16,11 @@ for line in f:
         else:
             wf.append(int(event[j]))
     wfs.append(wf[2:])
-    break
-fig, ax = plt.subplots(figsize=(8,16))
-for i in range(len(wfs)):
-    ax.plot(np.arange(len(wfs[i]))/0.75, wfs[i] - np.mean(wfs[i]) + i*50, label='ch' + str(i))
-ax.legend()
-ax.set_xlabel('Time (ns)')
-ax.set_ylabel('ADC count')
-fig.savefig('test.png')
+    fig, ax = plt.subplots(figsize=(8,16))
+    for i in range(len(wfs)):
+        ax.plot(np.arange(len(wfs[i]))/0.75, wfs[i] - np.mean(wfs[i]) + i*50, label='ch' + str(i))
+    ax.legend()
+    ax.set_xlabel('Time (ns)')
+    ax.set_ylabel('ADC count')
+    fig.savefig('event_%d.png' % linenum)
+    linenum += 1
